c69de7d4e9ec12c9c90fad1cd9b93a789abece99
raysc <raysc@RYA>
2024-03-15 21:39:57 -0400

    Add project files.

---
+++ StackCollection/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace PriorityQueueExample
+{
+    class Priority
+    {
+        static void Main(string[] args)
+        {
+
+            Stack<string> players = new Stack<string>();
+
+
+            players.Push("Chris");
+            players.Push("James");
+            players.Push("Mark");
+            players.Push("Ethan");
+            players.Push("Brad");
+
+            if(players.Contains("Ethan"))
+            {
+                Console.WriteLine("Ethan is online");
+            }
+            else
+            {
+                Console.WriteLine("Ethan is offline");
+            }
+            
+            players.Pop();
+
+            Console.WriteLine($"There are {players.Count} players online.");
+
+
+            Console.WriteLine("Players currently online:");
+            foreach (string player in players)
+            {
+                Console.WriteLine(player);
+            }
+        }
+    }
+}
